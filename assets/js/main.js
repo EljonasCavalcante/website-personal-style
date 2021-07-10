985046907639fd6fbcb6dd531b0742ffb9656c78
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-07-10 16:44:10 -0300

    Feat: creating services structure and adjustment of javascript and css.

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,3 +56,23 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
+
+/*qualification tabs 
+const tabs = document.querySelectorAll('[data-torget]'),
+      tabsContent = document.querySelectorAll('[data-content]')
+
+tabsContent.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+      const target = document.querySelector(tab.dataset.target)
+      
+      tabContent.forEach(tabContent =>{
+          tabContent.classList.remove('qualification__ative')
+      })
+      target.classList.add('qualification__active')
+
+      tabs.forEach(tab =>{
+          tab.classList.remove('qualification__active')
+      })
+      tab.classList.add('qualification_active')
+    })
+})*/
