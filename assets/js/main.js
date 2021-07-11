33575f110e0cfdfe773fbb8aea92797e80954667
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-07-10 22:18:55 -0300

    Feat:Creating function to open and close modal .

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,27 @@ tabsContent.forEach(tab =>{
       tab.classList.add('qualification_active')
     })
 })*/
+
+/* ==============service modal=========== */
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses =document.querySelectorAll('.services__modal-close')
+
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) =>{
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener('click', () =>{
+       modalViews.forEach((modalView) =>{
+          modalView.classList.remove('active-modal') 
+       }) 
+    })
+})
