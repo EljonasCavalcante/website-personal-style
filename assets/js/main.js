3d8c581c1f482a7e0cac06597e81c4d792c202b6
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-07-12 21:40:45 -0300

    Feat: Creating scroll up and style.

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,4 +158,13 @@ function scrollHeader(){
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll',scrollHeader)
-  /*======scroll sections active link================= */
+
+  /*======show scroll Up================= */
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+
+    if(this.scrollY >= 560)scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp )
