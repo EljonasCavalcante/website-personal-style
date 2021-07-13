fe464ac9e8b61b1b21da6df10139debdd608d8f5
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-07-12 22:57:28 -0300

    Feat:creatingsetting dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -168,3 +168,28 @@ function scrollUp(){
     else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp )
+
+
+ /*============drak light theme========= */
+
+ const themeButton = document.getElementById('theme-button')
+ const darkTheme = 'dark-theme'
+ const iconTheme = 'uil-sun'
+
+ const selectedTheme = localStorage.getItem('selected-theme')
+ const selectedIcon = localStorage.getItem('selected-icon')
+
+ const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+ if (selectedTheme) {
+     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+     themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+ }
+
+ themeButton.addEventListener('click', () =>{
+     document.body.classList.toggle(darkTheme)
+     themeButton.classList.toggle(iconTheme)
+     localStorage.setItem('selected-theme', getCurrentTheme())
+     localStorage.setItem('selected-icon', getCurrentIcon())
+ })
